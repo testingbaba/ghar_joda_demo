65b2e81aeed030ebbc07e501f0743db839314c1d
mohdrafeek <mohdfaruk70170@gmail.com>
2022-09-02 16:50:09 +0530

    by mohd faruk

---
+++ database/conn_db.php
@@ -0,0 +1,9 @@
+<?php
+$con = mysqli_connect("localhost", "root", "admin", "gharjoda_db");
+if (mysqli_connect_errno()) {
+    echo "Failed to connect to MySQL: " . mysqli_connect_error();
+    die();
+}
+date_default_timezone_set('Asia/Kolkata');
+$error = "";
+?>
